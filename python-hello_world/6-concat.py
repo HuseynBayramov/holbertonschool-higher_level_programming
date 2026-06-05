c2a995fe9c286dfccb3218a65a80a25a91ee0806
HuseynBayramov <palatqatoyu@gmail.com>
2026-06-05 00:09:00 +0000

    Fix: Final correct initial string values with exact line match

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Battery"
-str2 = "Street"
-str1 += (" " + str2)
-print("Welcome to {}!".format(str1))
+str1 = "Welcome to "
+str2 = "Holberton School"
+str1 += str2 + "!"
+print(f"{str1}")
