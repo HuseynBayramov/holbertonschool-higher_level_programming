004719cf59e5a0ff0dece8ff47c7a4589e5c4101
HuseynBayramov <palatqatoyu@gmail.com>
2026-06-20 15:28:16 +0400

    Create 102-square.py

---
+++ python-classes/102-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with comparison operators."""
+
+
+class Square:
+    """Class that represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Equal comparison."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Not equal comparison."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Less than comparison."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Less than or equal comparison."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Greater than comparison."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Greater than or equal comparison."""
+        return self.area() >= other.area()
